5401dfe12fb4d491752b216aabb2004bd290e2a0
sreenivasuluchilakuri-debug <sreenivasuluchilakuri@markwave.ai>
2026-01-08 10:14:43 +0530

    customtextfield

---
+++ lib/features/employee/presentation/widgets/custom_Textfield.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String? hint;
+  final String? initialValue;
+  final bool enabled;
+  final int maxLines;
+  final TextEditingController? controller;
+  final TextInputType keyboardType;
+  final FormFieldValidator<String>? validator;
+  final Widget? prefixIcon;
+  final TextStyle? style;
+
+  const CustomTextField({
+    super.key,
+    this.hint,
+    this.initialValue,
+    this.enabled = true,
+    this.maxLines = 1,
+    this.controller,
+    this.keyboardType = TextInputType.text,
+    this.validator,
+    this.prefixIcon,this.style,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      initialValue: controller == null ? initialValue : null,
+      enabled: enabled,
+      maxLines: maxLines,
+      keyboardType: keyboardType,
+      validator: validator,
+      style: style,
+      decoration: InputDecoration(
+        hintText: hint,
+        prefixIcon: prefixIcon,
+        filled: true,
+        fillColor: Colors.grey[100],
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 14,
+          vertical: 14,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
